1603d174cb2ff36468086d607539b3bb466e827f
Dhruv Thaker <167208515+dhruvthak3r@users.noreply.github.com>
2024-11-25 16:25:23 +0530

    Update Infixtopostfix.c

--- Stacks/Infixtopostfix.c
+++ Stacks/Infixtopostfix.c
@@ -1,103 +1,160 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
-#define max 100
-char stack[max];
-bool isEmpty();
-bool isFull();
-void infixtopostfix(char infix[],char postfix[]);
-void push(char symbol);
-char peek();
-char pop();
-int precedence(char symbol);
-int top=-1;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h>
 
-int main(){
-    char infix[max],postfix[max];
-    printf("Enter infix expression:\n");
+#define max 100
+
+char stack[max];
+int top = -1;
+char infix[max];
+char postfix[max];
+
+bool isFull();
+bool isEmpty();
+void push(char symbol);
+char pop();
+void infixtopostfix();
+char peek();
+int precedence(char symbol);
+int evaluatePostfix();
+
+int main() {
+    printf("Enter the infix expression:\n");
     fgets(infix, max, stdin);
-    infixtopostfix(infix, postfix);
-    printf("Postfix expression is: %s\n", postfix);
+    infixtopostfix();
+    printf("The postfix expression is: %s\n", postfix);
+    printf("The result of the postfix evaluation is: %d\n", evaluatePostfix());
+    return 0;
 }
 
-bool isFull(){
+bool isFull() {
     return top == max - 1;
 }
 
-bool isEmpty(){
+bool isEmpty() {
     return top == -1;
 }
 
-void infixtopostfix(char infix[], char postfix[]){
-    char symbol;
-    int index = 0;
-    for(int i = 0; i < strlen(infix); i++){
-        symbol = infix[i];
-        switch(symbol){
-            case '(':
-                push(symbol);
-                break;
-            case ')':
-                while(!isEmpty() && peek() != '('){
-                    postfix[index++] = pop();
-                }
-                pop(); // Remove '(' from stack
-                break;
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '%':
-            case '^':
-                while(!isEmpty() && precedence(stack[top]) >= precedence(symbol)){
-                    postfix[index++] = pop();
-                }
-                push(symbol);
-                break;
-            default:
-                postfix[index++] = symbol;
-        }
-    }
-    while(!isEmpty()){
-        postfix[index++] = pop();
-    }
-    postfix[index] = '\0';
-}
-
-void push(char symbol){
-    if(isFull()){
-        printf("Stack is full\n");
-        return;
+void push(char symbol) {
+    if (isFull()) {
+        printf("The stack is full\n");
+        exit(1);
     }
     stack[++top] = symbol;
 }
 
-char pop(){
-    if(isEmpty()){
-        printf("Stack is empty\n");
+char pop() {
+    if (isEmpty()) {
+        printf("The stack is empty\n");
         exit(1);
     }
     return stack[top--];
 }
 
-char peek(){
-    if(isEmpty()){
+void infixtopostfix() {
+    int index = 0;
+    for (int i = 0; i < strlen(infix); i++) {
+        char curr = infix[i];
+        if (curr == ' ' || curr == '\n') continue;
+        if (isdigit(curr)) {
+            postfix[index++] = curr;
+        } else {
+            switch (curr) {
+                case '(':
+                    push(curr);
+                    break;
+                case ')':
+                    while (!isEmpty() && peek() != '(') {
+                        postfix[index++] = pop();
+                    }
+                    pop();
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                case '^':
+                    while (!isEmpty() && precedence(peek()) >= precedence(curr)) {
+                        postfix[index++] = pop();
+                    }
+                    push(curr);
+                    break;
+                default:
+                    printf("Invalid character encountered\n");
+                    exit(1);
+            }
+        }
+    }
+    while (!isEmpty()) {
+        postfix[index++] = pop();
+    }
+    postfix[index] = '\0';
+}
+
+char peek() {
+    if (isEmpty()) {
         printf("Stack is empty\n");
         exit(1);
     }
     return stack[top];
 }
 
-int precedence(char symbol){
-    switch(symbol){
-        case '(': return 0;
+int precedence(char symbol) {
+    switch (symbol) {
+        case '(':
+            return 0;
         case '+':
-        case '-': return 1;
+        case '-':
+            return 1;
         case '*':
         case '/':
-        case '%': return 2;
-        case '^': return 3;
-        default: return 0;
+        case '%':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
     }
+}
+
+int evaluatePostfix() {
+    int evalStack[max];
+    int evalTop = -1;
+    for (int i = 0; i < strlen(postfix); i++) {
+        char curr = postfix[i];
+        if (isdigit(curr)) {
+            evalStack[++evalTop] = curr - '0';
+        } else {
+            int val2 = evalStack[evalTop--];
+            int val1 = evalStack[evalTop--];
+            switch (curr) {
+                case '+':
+                    evalStack[++evalTop] = val1 + val2;
+                    break;
+                case '-':
+                    evalStack[++evalTop] = val1 - val2;
+                    break;
+                case '*':
+                    evalStack[++evalTop] = val1 * val2;
+                    break;
+                case '/':
+                    evalStack[++evalTop] = val1 / val2;
+                    break;
+                case '%':
+                    evalStack[++evalTop] = val1 % val2;
+                    break;
+                case '^':
+                    evalStack[++evalTop] = (int)pow(val1, val2);
+                    break;
+                default:
+                    printf("Invalid operator encountered\n");
+                    exit(1);
+            }
+        }
+    }
+    return evalStack[evalTop];
 }
