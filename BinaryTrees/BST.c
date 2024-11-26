a19fd3595410e3541964af778fac26cbb08d5b2d
Dhruv Thaker <167208515+dhruvthak3r@users.noreply.github.com>
2024-11-26 21:14:26 +0530

    Update BST.c

--- BinaryTrees/BST.c
+++ BinaryTrees/BST.c
@@ -1,183 +1,141 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct treenode {
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    struct node *left;
+    struct node *right;
     int val;
-    struct treenode *left;
-    struct treenode *right;
 };
-
-struct treenode* createnode(int value);
-struct treenode* insert(struct treenode *root, int value);
-struct treenode* deleteNode(struct treenode *root, int value);
-void preorder(struct treenode *root);
-void inorder(struct treenode *root);
-void postorder(struct treenode *root);
-int countnodes(struct treenode *root);
-void leafnodes(struct treenode *root);
-int height(struct treenode *root);
-int max(int a, int b);
-struct treenode* mirror(struct treenode *node);
-int leafnode = 0;
-int main() {
-    struct treenode *root = NULL;
-    int choice, data, rootval;
-    printf("Enter the value of root: ");
-    scanf("%d", &rootval);
-    root = createnode(rootval);
-    while (1) {
-        printf("\n1. Insert into BST\n");
-        printf("2. Delete from BST\n");
-        printf("3. Preorder traversal\n");
-        printf("4. Inorder traversal\n");
-        printf("5. Postorder traversal\n");
-        printf("6. Count number of nodes \n");
-        printf("7. Count number of external nodes\n");
-        printf("8. Count number of internal nodes\n");
-        printf("9. Count height \n");
-        printf("10. Mirror the tree \n");
-        printf("11. Quit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        
-        if (choice == 11) break;
-
-        switch (choice) {
-            case 1:
-                printf("Enter the value of the node you want: ");
-                scanf("%d", &data);
-                root = insert(root, data);
-                break;
-            case 2:
-                printf("Enter the value of the node to delete: ");
-                scanf("%d", &data);
-                root = deleteNode(root, data);
-                break;
-            case 3:
-                printf("Preorder Traversal: ");
-                preorder(root);
-                break;
-            case 4:
-                printf("Inorder Traversal: ");
-                inorder(root);
-                break;
-            case 5:
-                printf("Postorder Traversal: ");
-                postorder(root);
-                break;
-            case 6:
-                printf("The number of nodes is %d\n", countnodes(root));
-                break;
-            case 7:
-                leafnode = 0;
-                leafnodes(root);
-                printf("The number of external nodes is %d\n", leafnode);
-                break;
-            case 8:
-                printf("The number of internal nodes is %d\n", countnodes(root) - leafnode);
-                break;
-            case 9:
-                printf("The height of the tree is: %d\n", height(root));
-                break;
-            case 10:
-                printf("The mirrored tree is:\n");
-                struct treenode* copy = mirror(root);
-                inorder(copy);
-                break;
-        }
-    }
-    return 0;
+struct node *createnode(int x){
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node ->val = x;
+    new_node ->left = NULL;
+    new_node -> right = NULL;
+    return new_node;
 }
-struct treenode* createnode(int value) {
-    struct treenode *newnode = (struct treenode *)malloc(sizeof(struct treenode));
-    newnode->val = value;
-    newnode->left = NULL;
-    newnode->right = NULL;
-    return newnode;
-}
-struct treenode* insert(struct treenode *root, int value) {
-    if (root == NULL) {
-        return createnode(value);
+struct node *insert(struct node *root,int x){
+    if(root == NULL){
+        return createnode(x);
     }
-    if (value < root->val) {
-        root->left = insert(root->left, value);
-    } else if (value > root->val) {
-        root->right = insert(root->right, value);
-    } else {
-        printf("The node is already present\n");
+    if(x > root->val){
+        root -> right = insert(root-> right , x);
+    }
+    else{
+        root -> left = insert(root -> left , x);
     }
     return root;
 }
-struct treenode* deleteNode(struct treenode *root, int value) {
-    if (root == NULL) return root;
-
-    if (value < root->val) {
-        root->left = deleteNode(root->left, value);
-    } else if (value > root->val) {
-        root->right = deleteNode(root->right, value);
-    } else {
-        if (root->left == NULL) {
-            struct treenode *temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            struct treenode *temp = root->left;
-            free(root);
-            return temp;
+struct node *delete(struct node *root,int x){
+    if(root == NULL){
+        return NULL;
+    }
+    if(x < root->val){
+        root -> left = delete(root -> left,x);
+    }
+    else if(x > root-> val){
+        root -> right = delete(root -> right,x);
+    }
+    else{
+        if(root -> left == NULL) return root -> right;
+        else if(root -> right == NULL) return root -> left;
+        else{
+            struct node *min = root -> right;
+            while(min -> left !=NULL){
+                min = min -> left;
+            }
+            root -> val = min -> val;
+            root -> right = delete(root -> right,min -> val);
         }
-        struct treenode *temp = root->right;
-        while (temp && temp->left != NULL) {
-            temp = temp->left;
-        }
-        root->val = temp->val;
-        root->right = deleteNode(root->right, temp->val);
     }
     return root;
 }
-
-void preorder(struct treenode *root) {
-    if (root == NULL) return;
-    printf("%d ", root->val);
-    preorder(root->left);
-    preorder(root->right);
+void preorder(struct node *root){
+    if(root == NULL) return;
+    printf("%d\n",root -> val);
+    preorder(root -> left);
+    preorder(root -> right);
 }
-void inorder(struct treenode *root) {
-    if (root == NULL) return;
-    inorder(root->left);
-    printf("%d ", root->val);
-    inorder(root->right);
+void inorder(struct node *root){
+    if(root == NULL) return;
+    inorder(root -> left);
+    printf("%d\n",root->val);
+    inorder(root -> right);
 }
-void postorder(struct treenode *root) {
-    if (root == NULL) return;
-    postorder(root->left);
-    postorder(root->right);
-    printf("%d ", root->val);
+void postorder(struct node *root){
+    if(root == NULL) return;
+    postorder(root -> left);
+    postorder(root -> right);
+    printf("%d\n",root -> val);
 }
-int countnodes(struct treenode *root) {
-    if (root == NULL) return 0;
-    return 1 + countnodes(root->left) + countnodes(root->right);
+int height(struct node *root){
+    if(root == NULL) return 0;
+    int left = height(root -> left);
+    int right = height(root -> right);
+    return 1 + max(left,right);
 }
-void leafnodes(struct treenode *root) {
-    if (root == NULL) return;
-    if (root->left == NULL && root->right == NULL) leafnode++;
-    leafnodes(root->left);
-    leafnodes(root->right);
-}
-int height(struct treenode *root) {
-    if (root == NULL) return 0;
-    int left = height(root->left);
-    int right = height(root->right);
-    return 1 + max(left, right);
-}
-int max(int a, int b) {
-    if(a >= b) return a;
+int max(int a ,int b){
+    if(a>=b) return a;
     return b;
 }
-struct treenode* mirror(struct treenode *node) {
-    if (node == NULL) return NULL;
-    struct treenode *left = mirror(node->left);
-    struct treenode *right = mirror(node->right);
-    node->left = right;
-    node->right = left;
-    return node;
+int leafnodes(struct node *root){
+    if(root == NULL) return 0;
+    if(root ->left ==NULL&& root -> right == NULL) return 1;
+    return leafnodes(root -> left) + leafnodes(root -> right);
 }
+struct node *mirror(struct node *root){
+    if(root == NULL) return NULL;
+    struct node *left = mirror(root -> left);
+    struct node *right = mirror(root -> right);
+    root -> right = left;
+    root -> left = right;
+    return root;
+}
+int main(){
+    int choice,x;
+    struct node *root = NULL;
+    while(1){
+        printf("1.insert\n");
+        printf("2.delete\n");
+        printf("3.height\n");
+        printf("4.mirror\n");
+        printf("5.count leafnodes\n");
+        printf("6.inorder\n");
+        printf("7.preorder\n");
+        printf("8.postorder\n");
+        printf("9.quit\n");
+        printf("enter your choice\n");
+        scanf("%d",&choice);
+        if(choice == 9) break;
+        switch(choice){
+            case 1:
+            printf("enter the value to insert\n");
+            scanf("%d",&x);
+           root = insert(root,x);
+            break;
+            case 2:
+            printf("enter the value to be deleted\n");
+            scanf("%d",&x);
+            root = delete(root,x);
+            break;
+            case 3:
+            printf("the height of the tree is : %d\n",height(root));
+            break;
+            case 4:
+            printf("the mirrored tree is : \n");
+            inorder(mirror(root));
+            break;
+            case 5:
+            printf("the number of leaf nodes is %d\n",leafnodes(root));
+            break;
+            case 6:
+            inorder(root);
+            break;
+            case 7:
+            preorder(root);
+            break;
+            case 8:
+            postorder(root);
+            break;
+        }
+    }
+}
+
